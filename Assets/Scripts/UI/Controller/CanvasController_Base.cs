2790dcfac12f7ebb5104c1d64e2afd7806ee1a94
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-04-23 22:12:51 +0900

    BaseCCを作成

--- Assets/Scripts/UI/Controller/CanvasController_Base.cs
+++ Assets/Scripts/UI/Controller/CanvasController_Base.cs
@@ -1,9 +1,30 @@
+using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// バトルのベース画面のキャンバスコントローラー
 /// </summary>
 public class CanvasController_Base : WindowBase
 {
+    [SerializeField] private Button _pauseButton;
     
+    public event Action OnPauseButtonClicked;
+
+    public override UniTask OnUIInitialize()
+    {
+        if (_pauseButton != null) _pauseButton.onClick.AddListener(OpenPausePanel);
+        return base.OnUIInitialize();
+    }
+
+    /// <summary>
+    /// ポーズパネルを開く
+    /// </summary>
+    private void OpenPausePanel() => OnPauseButtonClicked?.Invoke();
+
+    private void OnDestroy()
+    {
+        if(_pauseButton != null) _pauseButton.onClick?.RemoveAllListeners();
+    }
 }

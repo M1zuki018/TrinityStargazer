d06008318dc9423a649873f23989f22e13aea7df
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-04-24 17:56:50 +0900

    Factoryクラス編集

--- Assets/Scripts/Item/ItemFactory.cs
+++ Assets/Scripts/Item/ItemFactory.cs
@@ -1,9 +1,33 @@
-using UnityEngine;
+using System;
 
 /// <summary>
 /// アイテムファクトリー
 /// </summary>
 public class ItemFactory
 {
-    
+    /// <summary>
+    /// アイテムのインスタンスを作成する
+    /// </summary>
+    public ItemBase CreateItem(ItemTypeEnum type, RarityEnum rarity)
+    {
+        switch (type)
+        {
+            case ItemTypeEnum.SealPage:
+                return new SealPage(rarity);
+            case ItemTypeEnum.ReverseBroom:
+                return new ReverseBroom(rarity);
+            case ItemTypeEnum.CelestialForecast:
+                return new CelestialForecast(rarity);
+            case ItemTypeEnum.SmartPhone:
+                return new SmartPhone(rarity);
+            case ItemTypeEnum.ResonanceLight:
+                return new ResonanceLight(rarity);
+            case ItemTypeEnum.ChallengeRose:
+                return new ChallengeRose(rarity);
+            case ItemTypeEnum.StarAttraction:
+                return new StarAttraction(rarity);
+            default:
+                throw new ArgumentException("未知のアイテムタイプです");
+        }
+    }
 }

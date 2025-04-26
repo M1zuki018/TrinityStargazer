d3617dd67113906dcf26cebf787f24090ebf5d1f
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-04-27 00:09:04 +0900

    アイテム側の実装

--- Assets/Scripts/Item/Item/ChallengeRose.cs
+++ Assets/Scripts/Item/Item/ChallengeRose.cs
@@ -1,22 +1,46 @@
-using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 /// <summary>
 /// 「決闘の薔薇/ChallengeRose」：次の1回、相手が選んだ方向と自分が選んだ方向が一致すると必ず勝てる
 /// </summary>
 public class ChallengeRose : ItemBase
 {
+    // レアリティごとの効果をまとめた辞書
+    private static Dictionary<RarityEnum, int> RarityEffects = new()
+    {
+        { RarityEnum.R,   1},
+        { RarityEnum.SR,  2},
+        { RarityEnum.SSR, 5} // 最大ターン数は変動があるので初期化時に書き換える
+    };
+    
+    private int _getWinPoint;
+    
     public ChallengeRose(RarityEnum rarity) : base(rarity, ItemTypeEnum.SealPage)
     {
-        Name = "";
+        Name = "決闘の薔薇";
+        EffectSetting(rarity);
+        Description = $"使用したターンで勝利したとき{_getWinPoint}ポイント獲得する";
     }
 
     public override IItemEffect CreateEffect()
     {
-        throw new System.NotImplementedException();
+        return new ChallengeRoseEffect(_getWinPoint);
     }
 
-    public override void Use(IBattleMediator battleMediator)
+    /// <summary>
+    /// 効果を設定する
+    /// </summary>
+    private void EffectSetting(RarityEnum rarity)
     {
-        throw new System.NotImplementedException();
+        ModeData currentGameMode = GameManagerServiceLocator.Instance.GetGameModeData();
+        RarityEffects[RarityEnum.SSR] = currentGameMode.MaxTurn;
+        
+        if (!RarityEffects.TryGetValue(rarity, out var effects))
+        {
+            throw new ArgumentException($"未知のレアリティです: {rarity}");
+        }
+        
+        _getWinPoint = effects;
     }
 }

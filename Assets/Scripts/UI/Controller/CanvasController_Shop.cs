ba335f9ba5b62ca42930233be2ea961f6d585e80
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-04-27 21:13:52 +0900

    Shopのキャンバスコントローラーの実装

--- Assets/Scripts/UI/Controller/CanvasController_Shop.cs
+++ Assets/Scripts/UI/Controller/CanvasController_Shop.cs
@@ -9,12 +9,22 @@ using UnityEngine.UI;
 public class CanvasController_Shop : WindowBase
 {
     [SerializeField] private Button _closeButton;
+    [SerializeField] private Button _purchaseButton;
+    [SerializeField] private Button _saleButton;
+    [SerializeField] private Button _synthesisButton;
     
     public event Action OnHomeButtonClicked; // ホーム画面に戻る
+    public event Action OnPurchaseButtonClicked;
+    public event Action OnSaleButtonClicked;
+    public event Action OnSynthesisButtonClicked;
 
     public override UniTask OnUIInitialize()
     {
         if (_closeButton != null) _closeButton.onClick.AddListener(BackHome);
+        if(_purchaseButton != null) _purchaseButton.onClick.AddListener(PurchaseButtonClicked);
+        if(_saleButton != null) _saleButton.onClick.AddListener(SaleButtonClicked);
+        if(_synthesisButton != null) _synthesisButton.onClick.AddListener(SynthesisButtonClicked);
+        
         return base.OnUIInitialize();
     }
     
@@ -22,9 +32,27 @@ public class CanvasController_Shop : WindowBase
     /// モード選択画面→ホーム画面に遷移する
     /// </summary>
     private void BackHome() => OnHomeButtonClicked?.Invoke();
+    
+    /// <summary>
+    /// アイテム購入パネルを開く
+    /// </summary>
+    private void PurchaseButtonClicked() => OnPurchaseButtonClicked?.Invoke();
+    
+    /// <summary>
+    /// アイテム売却パネルを開く
+    /// </summary>
+    private void SaleButtonClicked() => OnSaleButtonClicked?.Invoke();
+    
+    /// <summary>
+    /// アイテム合成パネルを開く
+    /// </summary>
+    private void SynthesisButtonClicked() => OnSynthesisButtonClicked?.Invoke();
 
     private void OnDestroy()
     {
-        if(_closeButton != null) _closeButton.onClick.RemoveAllListeners();
+        if(_closeButton != null) _closeButton.onClick?.RemoveAllListeners();
+        if(_purchaseButton != null) _purchaseButton.onClick?.RemoveAllListeners();
+        if(_saleButton != null) _saleButton.onClick?.RemoveAllListeners();
+        if(_synthesisButton != null) _synthesisButton.onClick?.RemoveAllListeners();
     }
 }

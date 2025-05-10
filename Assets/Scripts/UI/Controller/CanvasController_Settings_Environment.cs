fff58c94fc22c4cd389ebc08bb99e97faee44ad1
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-11 00:16:35 +0900

    環境設定画面のクラスを作成

--- Assets/Scripts/UI/Controller/CanvasController_Settings_Environment.cs
+++ Assets/Scripts/UI/Controller/CanvasController_Settings_Environment.cs
@@ -1,6 +1,7 @@
 using System;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 /// <summary>
@@ -9,12 +10,10 @@ using UnityEngine.UI;
 public class CanvasController_Settings_Environment : WindowBase
 {
     [SerializeField] private Button _closeButton;
-    
-    [Header("言語")]
-    [SerializeField] private Button _textJapanese;
-    [SerializeField] private Button _textEnglish;
-    [SerializeField] private Button _voiseJapanese;
-    [SerializeField] private Button _voiseEnglish;
+
+    [Header("言語")] 
+    [SerializeField] private Button[] _textLanguage;
+    [SerializeField] private Button[] _voiceLanguage;
 
     [Header("シナリオ")]
     [SerializeField] private Button _scenarioSpeedLeft;
@@ -23,17 +22,61 @@ public class CanvasController_Settings_Environment : WindowBase
     [SerializeField] private Button _donnotUseAute;
     
     public event Action OnCloseButtonClicked;
+    public event Action<int> OnTextLanguageChanged; // テキスト言語を変更
+    public event Action<int> OnVoiceLanguageChanged; // ボイス言語を変更
+    public event Action<int> OnScenarioSpeedChanged; // シナリオ再生速度を変更
+    public event Action<bool> OnUseAutoChanged; // オート再生をするか変更する
+    
 
     public override UniTask OnUIInitialize()
     {
         if(_closeButton != null) _closeButton.onClick.AddListener(OnCloseButtonClick);
+        if(_scenarioSpeedLeft != null) _scenarioSpeedLeft.onClick.AddListener(() => ScenarioSpeedChanged(-1));
+        if(_scenarioSpeedRight != null) _scenarioSpeedRight.onClick.AddListener(() => ScenarioSpeedChanged(1));
+        if(_useAute != null) _useAute.onClick.AddListener(() => ScenarioAutoChanged(true));
+        if(_donnotUseAute != null) _donnotUseAute.onClick.AddListener(() => ScenarioAutoChanged(false));
+        RegisterLanguageButtons(_textLanguage, OnTextLanguageChanged);
+        RegisterLanguageButtons(_voiceLanguage, OnVoiceLanguageChanged);
         return base.OnUIInitialize();
     }
     
+    private void RegisterLanguageButtons(Button[] buttons, Action<int> callback)
+    {
+        if (buttons == null) return;
+        
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int index = i; // ループ変数をキャプチャするためのローカル変数
+            if (buttons[i] != null)
+                buttons[i].onClick.AddListener(() => callback?.Invoke(index));
+        }
+    }
+    
+    private void UnregisterButtonArray(Button[] buttons)
+    {
+        if (buttons == null) return;
+        
+        foreach (var button in buttons)
+        {
+            if (button != null)
+                button.onClick.RemoveAllListeners();
+        }
+    }
+    
     private void OnCloseButtonClick() => OnCloseButtonClicked?.Invoke();
+    
+    private void ScenarioSpeedChanged(int value) => OnScenarioSpeedChanged?.Invoke(value);
+    private void ScenarioAutoChanged(bool value) => OnUseAutoChanged?.Invoke(value);
 
     private void OnDestroy()
     {
         if(_closeButton != null) _closeButton.onClick?.RemoveAllListeners();
+        if(_scenarioSpeedLeft != null) _scenarioSpeedLeft.onClick?.RemoveAllListeners();
+        if(_scenarioSpeedRight != null) _scenarioSpeedRight.onClick?.RemoveAllListeners();
+        if(_useAute != null) _useAute.onClick?.RemoveAllListeners();
+        if(_donnotUseAute != null) _donnotUseAute.onClick?.RemoveAllListeners();
+        
+        UnregisterButtonArray(_textLanguage);
+        UnregisterButtonArray(_voiceLanguage);
     }
 }

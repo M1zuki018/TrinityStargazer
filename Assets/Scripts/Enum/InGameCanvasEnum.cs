ce2a444aa41f1be070cb7ac8736c7d9b03ab4a71
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-15 15:19:50 +0900

    enumを追加

---
+++ Assets/Scripts/Enum/InGameCanvasEnum.cs
@@ -0,0 +1,18 @@
+/// <summary>
+/// インゲームのキャンバスコントローラーのEnum
+/// </summary>
+public enum InGameCanvasEnum
+{
+    Title = 0,
+    Home = 1,
+    ModeSelect = 2,
+    Shop = 3,
+    ItemMenu = 4,
+    Settings = 5,
+    Credit = 6,
+    Quit = 7,
+    SettingsGraphic = 8,
+    SettingsSound = 9,
+    SettingsEnvironment = 10,
+    SettingsReset = 11
+}

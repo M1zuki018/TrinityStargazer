e6da8d9f4984eb5b8394c1274fa802fc93593027
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-27 13:24:53 +0900

    RuntimeLocalizeManagerから分離

---
+++ Assets/Scripts/Localize/RuntimeLocalizeItem.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine.UI;
+
+[Serializable]
+public class RuntimeLocalizeItem
+{
+    public Text textComponent;
+    public string japaneseText = "";
+    public string englishText = "";
+}

18da9f6a255a502c83fb6dd3ae81b11a889a8839
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-27 09:45:21 +0900

    UIElementBaseクラスを継承

--- Assets/Scripts/UI/UIElements/Title/LogoAnimation.cs
+++ Assets/Scripts/UI/UIElements/Title/LogoAnimation.cs
@@ -1,27 +1,21 @@
-using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine.UI;
 
 /// <summary>
 /// タイトル画面のロゴ用のアニメーション
 /// </summary>
-public class LogoAnimation :ViewBase
+public class LogoAnimation : UIElementBase<Image>
 {
-    private Image _image;
-    
-    public override UniTask OnStart()
+    private void Start()
     {
-        _image = GetComponent<Image>();
-        
         // 文字の浮遊アニメーション
         transform.DOMoveY(transform.position.y + 0.2f, 2f)
             .SetEase(Ease.InOutSine)
             .SetLoops(-1, LoopType.Yoyo);
     
         // 文字の輝きアニメーション
-        _image.DOFade(0.7f, 1.5f)
+        _element.DOFade(0.7f, 1.5f)
             .SetEase(Ease.InOutQuad)
             .SetLoops(-1, LoopType.Yoyo);
-        return base.OnStart();
     }
 }
--- Assets/Scripts/UI/UIElements/Title/VirsionText.cs
+++ Assets/Scripts/UI/UIElements/Title/VirsionText.cs
@@ -1,16 +1,12 @@
-using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
 /// タイトル画面のVersionのテキストを管理するクラス
 /// </summary>
-public class VirsionText : MonoBehaviour
+public class VirsionText : UIElementBase<Text>
 {
-    private Text _text;
-
     private void Start()
     {
-        _text = GetComponent<Text>();
-        _text.text = $"ver {GameData.VERSTION}";
+        _element.text = $"ver {GameData.VERTION}";
     }
 }

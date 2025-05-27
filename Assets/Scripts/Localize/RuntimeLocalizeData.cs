9026a2509110469db5b179be4f25e16514839e8e
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-27 13:25:05 +0900

    RuntimeLocalizeManagerから分離

---
+++ Assets/Scripts/Localize/RuntimeLocalizeData.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class RuntimeLocalizeData
+{
+    public List<RuntimeLocalizeGroup> groups = new List<RuntimeLocalizeGroup>();
+}

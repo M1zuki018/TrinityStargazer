2a7bc4f0fd661e0c44f58f46d5874987e8d7888c
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-27 13:25:00 +0900

    RuntimeLocalizeManagerから同じく分離

---
+++ Assets/Scripts/Localize/RuntimeLocalizeGroup.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class RuntimeLocalizeGroup
+{
+    public string groupName = "New Group";
+    public List<RuntimeLocalizeItem> items = new List<RuntimeLocalizeItem>();
+}

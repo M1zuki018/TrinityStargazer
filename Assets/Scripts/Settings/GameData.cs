23a0a27e71194292e5fe5d2e13533a6543da08ce
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-27 09:45:10 +0900

    タイポ修正

--- Assets/Scripts/Settings/GameData.cs
+++ Assets/Scripts/Settings/GameData.cs
@@ -3,6 +3,6 @@
 /// </summary>
 public static class GameData
 {
-    public const string VERSTION = "1.0.0";
+    public const string VERTION = "1.0.0";
     public const string SNS_URL = "https://x.com/kuon0108_";
 }
